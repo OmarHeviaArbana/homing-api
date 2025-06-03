0d5816bcd237439fff2d78a746c81535fe1b611a
oHeviaDeveloper <ohevia@uoc.edu>
2025-06-03 21:20:35 +0200

    Modified name seeder

--- database/seeders/DataBaseSeeder.php
+++ database/seeders/DataBaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class DataBaseSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
